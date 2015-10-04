4d2c4307a9f226a08beea24f9dda7b4634ce922e
Kiyoon Ko <kiyoonko@gmail.com>
2015-10-04 00:38:08 -0400

    Fixed R.java proble,

--- app/src/main/java/com/gmail/kiyoonko/virms/MainActivity.java
+++ app/src/main/java/com/gmail/kiyoonko/virms/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.gmail.kiyoonko.virms;
 
 import android.annotation.SuppressLint;
@@ -23,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.gmail.kiyoonko.virms.R;
 
 import com.google.atap.tangoservice.Tango;
 import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
