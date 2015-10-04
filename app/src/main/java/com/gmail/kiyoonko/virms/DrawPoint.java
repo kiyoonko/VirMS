1a6138e977051167084ab329f06c126f509eb095
Jaehyeong <jaehyeong.lee@gmail.com>
2015-10-04 02:43:48 -0500

    Updated DrawPoint
    Added toucheventlistener

--- app/src/main/java/com/gmail/kiyoonko/virms/DrawPoint.java
+++ app/src/main/java/com/gmail/kiyoonko/virms/DrawPoint.java
@@ -1,12 +1,23 @@
 package com.gmail.kiyoonko.virms;
 
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener
+
 /**
  * Created by JaehyeongLee on 10/3/15.
  */
-public class DrawPoint {
-    public DrawPoint(Vectors v) {
-        double x = v.getX()+0.1;
-        double y = v.getY()+0.1;
-        double z = v.getZ();
+
+public void class DrawPoint {
+    public OnTouchListener getListener(){
+        return new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                double x = v.getX()+0.1;
+                double y = v.getY()+0.1;
+                double z = v.getZ();
+                return true;
+            }
+        }
     }
 }
